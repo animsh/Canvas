14a34149becf11d28715c10fa7c4c0bf8b41a3f3
animsh <animsh.more@gmail.com>
2021-06-02 12:02:35 +0530

    changelog: created canvasView

---
+++ app/src/main/java/com/animsh/canvas/CanvasView.kt
@@ -0,0 +1,90 @@
+package com.animsh.canvas
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+/**
+ * Created by animsh on 6/2/2021.
+ */
+class CanvasView(
+    context: Context,
+    attributeSet: AttributeSet
+) : View(context, attributeSet) {
+
+    private var mDrawPath: CustomPath? = null
+    private var mCanvasBitmap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var mBrushSize: Float = 0.toFloat()
+    private var mColor = Color.BLACK
+    private var canvas: Canvas? = null
+
+    init {
+        setupCanvasView()
+    }
+
+    private fun setupCanvasView() {
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(mColor, mBrushSize)
+        mDrawPaint.apply {
+            this!!
+            color = mColor
+            style = Paint.Style.STROKE
+            strokeJoin = Paint.Join.ROUND
+            strokeCap = Paint.Cap.ROUND
+        }
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mBrushSize = 20.toFloat()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+        if (!mDrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath!!.color = mColor
+                mDrawPath!!.brushThickness = mBrushSize
+                mDrawPath!!.reset()
+                mDrawPath!!.moveTo(touchX!!, touchY!!)
+            }
+            MotionEvent.ACTION_MOVE -> {
+                mDrawPath!!.lineTo(touchX!!, touchY!!)
+            }
+            MotionEvent.ACTION_UP -> {
+                mDrawPath = CustomPath(mColor, mBrushSize)
+            }
+            else -> return false
+        }
+        invalidate()
+        return true
+
+    }
+
+    internal inner class CustomPath(
+        var color: Int,
+        var brushThickness: Float
+    ) : Path() {
+
+    }
+}
